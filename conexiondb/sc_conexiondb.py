4c35cc20ada292d68390ea96b617c4973f93db09
Luis Acevedo <laar@openmailbox.org>
2016-06-18 13:50:01 -0400

    agrega permisos de superusuario
    e le conceden permisos de superusuario depués de crear
    la base de datos db_canaima

--- conexiondb/sc_conexiondb.py
+++ conexiondb/sc_conexiondb.py
@@ -33,6 +33,7 @@ def createdb():
 	con, cur = conexion_open(conexion1)
 	con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 	cur.execute("create database db_canaima")
+	cur.execute("alter role %s with superuser" % (usuario,))
 	cur.close()
 	con.close()
 
@@ -53,7 +54,7 @@ conexion2 = "dbname=db_canaima user=%s password=%s" % (usuario, password)
 
 try:
 	con, cur = conexion_open(conexion1)
-	cur.execute("SELECT datname from pg_database WHERE datname = (%s)", ("db_canaima",))
+	cur.execute("select datname from pg_database where datname = (%s)", ("db_canaima",))
 	row = cur.fetchall()
 	con.close()
 except (NameError, TypeError):
